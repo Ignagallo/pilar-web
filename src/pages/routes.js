0c4060fa14666c598c811cfd254eadfc6ee3bf20
Ignacio Gallo <ignaciogalloprof@gmail.com>
2023-08-22 00:00:47 -0300

    Subiendo TP1

---
+++ src/pages/routes.js
@@ -0,0 +1,21 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+import DashboardLayout from "./layouts/dashboardLayout.jsx";
+import NotFound from "./layouts/notFound.jsx";
+import Todo from "./todo/todo.js"
+import Dashboard from "./dashboard/dashboard.jsx";
+
+const Routes = () => {
+    return useRoutes([
+        {
+            path: '/',
+            element: <DashboardLayout />,
+            children: [
+                { path: '', element: <Dashboard /> },
+            ]
+        },
+        { path: '/404', element: <NotFound /> },
+        { path: '*', element: <Navigate to="/404" replace /> }
+    ])
+}
+
+export default Routes;
